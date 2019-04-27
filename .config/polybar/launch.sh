be3fb59d6e203b2f480534f806642e97ce6022f5
Christopher Goh <chrisgzf@gmail.com>
2019-04-28 03:22:16 +0800

    Fix polybar showing on wrong monitor

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+MONITOR=$(polybar -m|tail -1|sed -e 's/:.*$//g')
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+echo $MONITOR
 # Launch bar1 and bar2
-polybar chrisbar &
+MONITOR=$MONITOR polybar chrisbar &
 
 echo "Bars launched..."
